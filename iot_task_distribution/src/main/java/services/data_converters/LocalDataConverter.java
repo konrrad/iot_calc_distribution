2233aed0ea9485778c5be90d74ea64688fe789bd
wolski0420 <wolski0420@gmail.com>
2021-01-01 10:33:11 +0100

    LoadDataConverter v2

--- iot_task_distribution/src/main/java/services/data_converters/LocalDataConverter.java
+++ iot_task_distribution/src/main/java/services/data_converters/LocalDataConverter.java
@@ -1,19 +1,23 @@
 package services.data_converters;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import model.Branch;
 
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class LocalDataConverter implements DataConverter {
+    private final Gson gson = new Gson();
+
     @Override
     public List<Branch> jsonToModel(String json) {
-        // TODO: 12/31/2020 not implemented by Łukasz
-        return null;
+        Type type = new TypeToken<List<Branch>>(){}.getType();
+        return gson.fromJson(json, type);
     }
 
     @Override
     public String modelToJson(List<Branch> branches) {
-        // TODO: 12/31/2020 not implemented by Łukasz
-        return null;
+        return gson.toJson(branches);
     }
 }
