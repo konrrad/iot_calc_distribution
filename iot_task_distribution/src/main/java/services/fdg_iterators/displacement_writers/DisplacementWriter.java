a8ffeb8441815e3128b93a9100365926eacffdda
szelemeh <subzarro6@gmail.com>
2021-01-03 16:15:53 +0200

    Fix return types

--- iot_task_distribution/src/main/java/services/fdg_iterators/displacement_writers/DisplacementWriter.java
+++ iot_task_distribution/src/main/java/services/fdg_iterators/displacement_writers/DisplacementWriter.java
@@ -3,7 +3,7 @@ package services.fdg_iterators.displacement_writers;
 import model.Vertex;
 
 public interface DisplacementWriter {
-    Void writeAttrDisplacement(Vertex v1, Vertex v2);
-    Void writeRepDisplacement(Vertex v1, Vertex v2);
-    Void updateLocation(Vertex v);
+    void writeAttrDisplacement(Vertex v1, Vertex v2);
+    void writeRepDisplacement(Vertex v1, Vertex v2);
+    void updateLocation(Vertex v);
 }
--- iot_task_distribution/src/main/java/services/fdg_iterators/displacement_writers/M2MDisplacementWriter.java
+++ iot_task_distribution/src/main/java/services/fdg_iterators/displacement_writers/M2MDisplacementWriter.java
@@ -4,20 +4,17 @@ import model.Vertex;
 
 public class M2MDisplacementWriter implements DisplacementWriter {
     @Override
-    public Void writeAttrDisplacement(Vertex v1, Vertex v2) {
+    public void writeAttrDisplacement(Vertex v1, Vertex v2) {
         // TODO: 12/31/2020 not implemented by Staszek
-        return null;
     }
 
     @Override
-    public Void writeRepDisplacement(Vertex v1, Vertex v2) {
+    public void writeRepDisplacement(Vertex v1, Vertex v2) {
         // TODO: 12/31/2020 not implemented by Staszek
-        return null;
     }
 
     @Override
-    public Void updateLocation(Vertex v) {
+    public void updateLocation(Vertex v) {
         // TODO: 12/31/2020 not implemented by Staszek
-        return null;
     }
 }
