4bdc1ca0c4a54886e427026fe56c26f1f2b0eec8
szelemeh <subzarro6@gmail.com>
2021-01-04 18:39:55 +0200

    Prettify json output

--- iot_task_distribution/src/main/java/services/data_converters/LocalDataConverter.java
+++ iot_task_distribution/src/main/java/services/data_converters/LocalDataConverter.java
@@ -1,6 +1,7 @@
 package services.data_converters;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import model.Branch;
 
@@ -8,7 +9,7 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public class LocalDataConverter implements DataConverter {
-    private final Gson gson = new Gson();
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Override
     public Branch jsonToModel(String json) {
